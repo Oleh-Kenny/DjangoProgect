4919ee829620ee09a5b5c4bf1cff11a103caf729
Oleh <lemeholeh@gmail.com>
2020-05-13 16:40:49 +0300

    moduls_blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogList
+
+class BlogListAdmin(admin.ModelAdmin):
+    list_display = ("name_list", "blog_date", "text_list", "carmanager", "rating")
+    list_display_links = ("name_list", "blog_date", "text_list", "carmanager", "rating")
+    search_fields = ("name_list", "blog_date", "text_list", "carmanager", "rating")
+    list_per_page = 20
+
+admin.site.register(BlogList, BlogListAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from carmanager.models import CarManager
+from datetime import datetime
 
-# Create your models here.
+
+class BlogList(models.Model):
+    photo_list = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    name_list = models.CharField(max_length=300)
+    blog_date = models.DateTimeField(default=datetime.now, blank=True)
+    carmanager = models.ForeignKey(CarManager, on_delete=models.DO_NOTHING)
+    text_list = models.TextField(blank=True)
+    visible_list = models.BooleanField(blank=True)
+    rating = models.IntegerField(default=True)
+
+    def __str__(self):
+        return self.name_list
